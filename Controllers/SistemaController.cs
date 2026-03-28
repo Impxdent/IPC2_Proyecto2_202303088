1a702824f5d4e9b36aa3a5b0fb1010ee52062b74
André Escobar <andreescobara@gmail.com>
2026-03-27 20:17:51 -0600

    Ventana de detalle en sistemas

--- Controllers/SistemaController.cs
+++ Controllers/SistemaController.cs
@@ -12,5 +12,25 @@ namespace IPC2_Proyecto2_202303088.Controllers
 
             return View(servicio.ListaSistemas);
         }
+        public IActionResult Detalle(string nombre)
+        {
+            var servicio = HomeController.servicio;
+
+            Nodo actual = servicio.ListaSistemas.ObtenerCabeza();
+
+            while (actual != null)
+            {
+                SistemaDrones sistema = (SistemaDrones)actual.Dato;
+
+                if (sistema.Nombre == nombre)
+                {
+                    return View(sistema);
+                }
+
+                actual = actual.Siguiente;
+            }
+
+            return Content("Sistema no encontrado");
+        }
     }
 }
--- obj/Debug/net10.0/IPC2_Proyecto2_202303088.AssemblyInfo.cs
+++ obj/Debug/net10.0/IPC2_Proyecto2_202303088.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2956322efe19f72aa8e86c1beb7595925549981e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f334b04c3c2c721580e7ba96615ae52f9859fce")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
