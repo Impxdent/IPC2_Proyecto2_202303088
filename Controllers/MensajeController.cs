04eda3d5274fc43fb88c1b793fb25dd47db23fe8
André Escobar <andreescobara@gmail.com>
2026-04-05 21:19:10 -0600

    Proyecto terminado

--- Controllers/MensajeController.cs
+++ Controllers/MensajeController.cs
@@ -44,14 +44,11 @@ namespace IPC2_Proyecto2_202303088.Controllers
                         while (actualInstruccion != null)
                         {
                             Instruccion inst = (Instruccion)actualInstruccion.Dato;
-                            
                             Nodo actualAltura = sistemaAsociado.Alturas.ObtenerCabeza();
-                            
                             while (actualAltura != null)
                             {
                                 Altura alt = (Altura)actualAltura.Dato;
-                                
-                                if (alt.Nivel == inst.Altura) 
+                                if (alt.Nivel == inst.Altura && alt.NombreDron == inst.Dron) 
                                 {
                                     mensajeDescifrado += alt.Letra;
                                     break;
@@ -60,6 +57,9 @@ namespace IPC2_Proyecto2_202303088.Controllers
                             }
                             actualInstruccion = actualInstruccion.Siguiente;
                         }
+
+                        ViewBag.DronesSistema = sistemaAsociado.Drones;
+                        ViewBag.TablaSimulacion = GenerarTablaSimulacion(mensaje, sistemaAsociado);
                     }
 
                     IPC2_Proyecto2_202303088.Servicios.Simulador simulador = new IPC2_Proyecto2_202303088.Servicios.Simulador();
@@ -67,18 +67,75 @@ namespace IPC2_Proyecto2_202303088.Controllers
 
                     IPC2_Proyecto2_202303088.Graphviz.Graficador graficador = new IPC2_Proyecto2_202303088.Graphviz.Graficador();
                     string rutaImagen = graficador.GraficarInstrucciones(mensaje);
+                    
                     ViewBag.RutaImagen = rutaImagen;
-
                     ViewBag.MensajeDescifrado = mensajeDescifrado;
                     ViewBag.TiempoOptimo = tiempoOptimo;
 
                     return View(mensaje);
                 }
-
                 actual = actual.Siguiente;
             }
-
             return Content("Mensaje no encontrado");
+        }
+
+        private ListaSimple GenerarTablaSimulacion(Mensaje m, SistemaDrones s)
+        {
+            ListaSimple simulacion = new ListaSimple();
+            int segundo = 1;
+            bool terminado = false;
+
+            while (!terminado)
+            {
+                terminado = true;
+                ListaSimple accionesDelSegundo = new ListaSimple();
+                Nodo nDron = s.Drones.ObtenerCabeza();
+
+                while (nDron != null)
+                {
+                    Dron d = (Dron)nDron.Dato;
+                    string accion = CalcularAccionDron(m, d.Nombre, segundo, out bool aunTrabaja);
+                    
+                    if (aunTrabaja) terminado = false;
+                    
+                    accionesDelSegundo.Insertar(new { Dron = d.Nombre, Accion = accion });
+                    nDron = nDron.Siguiente;
+                }
+
+                if (!terminado)
+                {
+                    simulacion.Insertar(new { Segundo = segundo, Acciones = accionesDelSegundo });
+                    segundo++;
+                }
+                if (segundo > 500) break;
+            }
+            return simulacion;
+        }
+
+        private string CalcularAccionDron(Mensaje m, string nombreDron, int segObjetivo, out bool trabajando)
+        {
+            int altActual = 0;
+            int segCronometro = 0;
+            Nodo n = m.Instrucciones.ObtenerCabeza();
+
+            while (n != null)
+            {
+                Instruccion i = (Instruccion)n.Dato;
+                if (i.Dron == nombreDron)
+                {
+                    while (altActual != i.Altura)
+                    {
+                        segCronometro++;
+                        if (segCronometro == segObjetivo) { trabajando = true; return altActual < i.Altura ? "Subir" : "Bajar"; }
+                        if (altActual < i.Altura) altActual++; else altActual--;
+                    }
+                    segCronometro++;
+                    if (segCronometro == segObjetivo) { trabajando = true; return "Emitir Luz"; }
+                }
+                n = n.Siguiente;
+            }
+            trabajando = false;
+            return "Esperar";
         }
     }
 }
--- obj/Debug/net10.0/IPC2_Proyecto2_202303088.AssemblyInfo.cs
+++ obj/Debug/net10.0/IPC2_Proyecto2_202303088.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529f4f520cdd7ae9452eaf5169ad7ef626b3073c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0953cbfe09d61504487c3e82d114259c65b5f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPC2_Proyecto2_202303088")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
